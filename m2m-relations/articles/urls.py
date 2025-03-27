e276aac398e7704183053c5e6fc141f34a2a6720
Ruslan Suleimanov <163177231+asasxa@users.noreply.github.com>
2025-03-27 20:56:02 +0300

    Update urls.py

--- m2m-relations/articles/urls.py
+++ m2m-relations/articles/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-
 from articles.views import articles_list
 
 urlpatterns = [
     path('', articles_list, name='articles'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
