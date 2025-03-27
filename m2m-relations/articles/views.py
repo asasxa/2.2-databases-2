2e323f296fa3072ac9dcd8afc0c3843b39156778
Ruslan Suleimanov <163177231+asasxa@users.noreply.github.com>
2025-03-27 20:53:03 +0300

    Update views.py

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -2,9 +2,5 @@ from django.shortcuts import render
 from .models import Article
 
 def articles_list(request):
-    template = 'articles/news.html'
     articles = Article.objects.prefetch_related('scopes__tag').order_by('-published_at')
-    context = {
-        'articles': articles,
-    }
-    return render(request, template, context)
+    return render(request, 'articles/news.html', {'object_list': articles})
